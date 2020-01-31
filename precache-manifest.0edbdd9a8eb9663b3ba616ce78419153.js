f24c55f842bc405c0a6b7a5a20125d3da56b8439
aleclofabbro <alessandro.giansanti@gmail.com>
2020-01-31 17:58:11 +0100

    Updates

---
+++ precache-manifest.0edbdd9a8eb9663b3ba616ce78419153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0873c30c1bc3b3af660e1fb81f6f3097",
+    "url": "/stickery/index.html"
+  },
+  {
+    "revision": "59fb04518a553f71305e",
+    "url": "/stickery/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6674123d0f9f16b61121",
+    "url": "/stickery/static/js/2.26df273d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/stickery/static/js/2.26df273d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59fb04518a553f71305e",
+    "url": "/stickery/static/js/main.f1ae069f.chunk.js"
+  },
+  {
+    "revision": "4eddffbf09df86999e0f",
+    "url": "/stickery/static/js/runtime-main.b1d48992.js"
+  }
+]);
